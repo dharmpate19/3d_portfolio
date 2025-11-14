2f4071b351036889a5434c53204be8f2fca28496
Dharm Patel <pdharm774@gmail.com>
2025-11-14 23:49:25 +0530

    Edited Island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import {Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
-import Island from '../models/island'
+import Island from '../models/Island'
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -28,11 +28,9 @@ const Home = () => {
             camera={{near : 0.1, far: 1000}}>
             
             <Suspense fallback={<Loader/>}>
-            <directionalLight/>
-            <ambientLight/>
-            <pointLight/>
-            <spotLight/>
-            <hemisphereLight/>
+            <directionalLight position={[1,1,1]} intensity={2}/>
+            <ambientLight intensity={0.5}/>
+            <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
             <Island position={islandPosition} scale={islandScale} rotation={islandRotation}/>
             </Suspense>
             </Canvas>
