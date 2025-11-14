b0128da09388fdbffe29da75532c50342beab7f1
Dharm Patel <pdharm774@gmail.com>
2025-11-15 00:22:50 +0530

    Create plane and bird 3d model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
 import  Sky  from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -35,12 +37,14 @@ const Home = () => {
             groundColor="#000000"
             intensity={1}
           />
+          <Bird/>
           <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
           />
+          <Plane/>
         </Suspense>
       </Canvas>
     </section>
