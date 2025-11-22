ef1f9d28bc1c75f0f9aad2562419ef662843c24d
Dharm Patel <pdharm774@gmail.com>
2025-11-23 02:33:34 +0530

    Add cions

---
+++ src/assets/icons/index.js
@@ -0,0 +1,85 @@
+import css from './css.svg'
+import express from './express.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import mongodb from './mongodb.svg'
+import motion from './motion.svg'
+import mui from './mui.svg'
+import nextjs from './nextjs.svg'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import redux from './redux.svg'
+import sass from './sass.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import car from './car.svg'
+import estate from './estate.svg'
+import pricewise from './pricewise.svg'
+import snapgram from './snapgram.svg'
+import summiz from './summiz.svg'
+import threads from './threads.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+import python from './python.png'
+import PostgreSQL from './postgresql.png'
+import MySQL from './mysql.png'
+import GraphQL from './graphql.png'
+import Docker from './docker.png'
+import Kubernetes from './kubernetes.png'
+import Jenkins from './jenkins.png'
+import GitLab from './gitlab.png'
+import AWS from './aws.png'
+import Vercel from './vercel.png'
+import PHP from './php.png'
+import wordpress from './wordpress.png'
+
+
+export {
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    react,
+    redux,
+    sass,
+    tailwindcss,
+    typescript,
+    linkedin,
+    twitter,
+    car,
+    estate,
+    pricewise,
+    snapgram,
+    summiz,
+    threads,
+    arrow,
+    contact,
+    soundon,
+    soundoff,
+    python,
+    PostgreSQL,
+    MySQL,
+    GraphQL,
+    Docker,
+    Kubernetes,
+    Jenkins,
+    GitLab,
+    AWS,
+    Vercel,
+    PHP,
+    wordpress,
+
+}
