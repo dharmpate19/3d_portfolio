244224ab3909fc40e291faed79b0e86ee488a17a
Dharm Patel <pdharm774@gmail.com>
2025-11-24 01:05:12 +0530

    make loader  effect

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,11 @@ const Loader = () => {
   return (
     <Html>
       <div className="flex justify-center items-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spins" />
+        <div className="relative w-20 h-20">
+          <div className="absolute inset-0 rounded-full border-4 border-blue-500 opacity-30 blur-lg animate-pulse" />
+          <div className="w-full h-full border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+          <div className="absolute top-3 left-3 right-3 bottom-3 border-4 border-blue-400 border-b-transparent rounded-full animate-[spin_1.2s_linear_reverse_infinite]" />
+        </div>
       </div>
     </Html>
   );
